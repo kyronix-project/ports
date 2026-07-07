374597fc31909b333a4cfaf9309ca6a52c59b60a
AnmiTaliDev <anmitali198@gmail.com>
2026-07-08 00:27:41 +0500

    feat: add openssl
    Closes #11

---
+++ libs/openssl/obj-musl/crypto/libcrypto-lib-array_alloc.d
@@ -0,0 +1,13 @@
+crypto/libcrypto-lib-array_alloc.o: ../crypto/array_alloc.c \
+ ../include/internal/mem_alloc_utils.h ../include/internal/common.h \
+ include/openssl/configuration.h ../include/internal/e_os.h \
+ ../include/openssl/opensslconf.h ../include/openssl/macros.h \
+ include/openssl/opensslv.h ../include/openssl/e_os2.h \
+ include/openssl/crypto.h include/openssl/safestack.h \
+ ../include/openssl/stack.h ../include/openssl/types.h \
+ ../include/openssl/cryptoerr.h ../include/openssl/symhacks.h \
+ ../include/openssl/cryptoerr_legacy.h ../include/openssl/core.h \
+ ../include/internal/numbers.h ../include/internal/nelem.h \
+ ../include/internal/safe_math.h include/openssl/err.h \
+ include/openssl/bio.h ../include/openssl/bioerr.h \
+ include/openssl/lhash.h
